428f357b03c3071241a677bc26f767fa9bd8ba66
loterini0 <jloteror@unal.edu.co>
2025-06-18 11:51:23 -0500

    Creacion de MCR

---
+++ prueba2/database/migrations/2025_06_18_164219_create_cars_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_type_car')->references('id')->on('Type_cars');
+            $table->foreignId('id_route')->references('id')->on('Routes');
+            $table->string('driver');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
